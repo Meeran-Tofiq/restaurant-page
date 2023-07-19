96ce62061dfd27a5545d81aab78fad48572e804b
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-07-19 15:07:03 +0300

    Refactor mode to development on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Thor\s grill',
-    }),
-  ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Thor\s grill',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
