d84ed4d671c0a8f1bef7a8ec4d533b2f8b7939f8
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-07-21 17:04:36 +0300

    Feat syling of main content of home page

--- src/load.home.js
+++ src/load.home.js
@@ -32,7 +32,7 @@ function loadHomePage() {
     navAbout.innerText = "About";
 
     imgCredits.innerHTML = 'Photo by <a href="https://unsplash.com/@evanthewise">Evan Wise</a>.';
-    infoDiv.innerHTML = "<h3>Lorem Ipsum es simplemente el texto de relleno<\\h3>" + 
+    infoDiv.innerHTML = "<h3>Lorem Ipsum es simplemente el texto de relleno</h3>" + 
     "de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta)" + 
     " desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.";
 
@@ -48,8 +48,8 @@ function loadHomePage() {
 
     imgContainerDiv.appendChild(imgGrill);
     imgContainerDiv.appendChild(imgCredits);
+    imgContainerDiv.appendChild(infoDiv);
     mainDiv.appendChild(imgContainerDiv);
-    mainDiv.appendChild(infoDiv);
 
     content.appendChild(header);
     content.appendChild(mainDiv);
